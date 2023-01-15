8d6f05b6cd412086c8313aa911fab9ecdf2459d2
Laura S <57412421+LauraS909@users.noreply.github.com>
2023-01-15 15:53:24 -0500

    I did so much here

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 """
 This program is intended to help me download and import erg data from concept2's website
 """
-import requests
+import requests, json
+from requests.exceptions import Timeout
+from getpass import getpass
 
-cred = { 'un': 'newton145', 
+cred = {'client_id':'x',
+'client_secret': 'x',
+'grant_type':'',
+'scope': 'user:read,results:read',
+'un': 'newton145', 
 'pw' : 'bvq8fvp2rza2RYQ@pep', 
 'url' : 'https://log.concept2.com/login',
 'dev_url': 'https://log-dev.concept2.com/login'
 }
 
-response = requests.get(cred['url'])
+form_data = {'username':'newton145', 'pw':'bvq8fvp2rza2RYQ@pep'}
 
-print(response)
+def user_login_c2(url):
+    with requests.Session() as s:
+        #session.auth('username', getpass())
+
+        r = s.get(url)
+        #read_r = r.json()
+        print(r.encoding)
+        print(r.headers)
+        r = s.post(url, data = form_data)
+        print(r.text)
+        return [s,r]
+
+
+[s, r] = user_login_c2(cred.get('url'))
