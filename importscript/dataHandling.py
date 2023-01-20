d0d1d5bcf0f045ee949d3fcc6bc9a1bc8de57b94
Laura S <57412421+LauraS909@users.noreply.github.com>
2023-01-19 20:46:19 -0500

    Getting rid of the weird parsing

--- importscript/dataHandling.py
+++ importscript/dataHandling.py
@@ -1,36 +1,30 @@
 import pandas as pd
-import csv
 from pathlib import Path
+
 
 
 
 int_header = ['login url', 'home url', 'username', 'password', 'csv path']
 user_header = ['name', 'logboook url']
 cred_path = Path.home().joinpath('Documents').joinpath('C2Logbook Downloader').joinpath('CSV').joinpath('internal.csv')
+rower_path = Path.home().joinpath('Documents').joinpath('C2Logbook Downloader').joinpath('CSV').joinpath('rowers.csv')
 default_home = Path.home().joinpath('Documents').joinpath('C2Logbook Downloader').joinpath('CSV')
 
-def openFormatting():
+def rewriteCredentials(un, pw, login_url = 'https://log.concept2.com/login', home_url = 'https://log.concept2.com/log',
+file_path = ''):
     path = cred_path.parent
     if not Path.exists(path):
         path.mkdir(parents=True)
-    csvfile = open(cred_path, mode='w')
-    writer = csv.DictWriter(csvfile, fieldnames=int_header)
-    writer.writeheader()
-    return writer
-
-
-
-def rewriteCredentials(un, pw, login_url = 'https://log.concept2.com/login', home_url = 'https://log.concept2.com/log',
-csv_path = ''):
-    w = openFormatting()
     cred = {
     'username': un, 
     'password' : pw, 
     'login url' : login_url,
     'home url': home_url,
-    'csv path': csv_path
+    'file path': file_path
     }
-    w.writerow(cred)
+    df = pd.DataFrame(cred)
+    df.to_csv(path)
+
 
 """
 Seeks the credentials from the file path
@@ -38,42 +32,58 @@ IF file doesn't exist, it returns False
 """
 def getCred(filepath = cred_path):
     try:
-       with open(filepath, newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            return row
+       df = pd.read_csv(filepath)
+       return df
     except:
         print('No credentials loaded')
         return False
 
-"""
-Creates the csv file and sets the proper headers
-"""
-def createFile(name, intervals, path):
-    #Format headers 
-    fieldnames = [name, ]
-    inters = numberIntervals(intervals)
-    for i in inters:
-        fieldnames.append(i)
-    csvfile = open(path.join(name).join('.csv'), 'w', newline='')
-    #Formats in excel tab format
-    w = csv.DictWriter(csvfile, dialect='excel_tab')
-    w.writeheader(fieldnames)
-    return w
 
 """
 Writes rower data as a row
-@param intervals: raw intervals 
+@param name: [str] name of rower
+@param data: [dict]
+@param df: pandas dataframe
+returns dataframe + rower as row
+
 """
-def writeRower(name, data, intervals, filename):
-    datalist = list()
-    datalist.append(name)
-    for d in data:
-        datalist.append(d)
-    #TODO: figure out where I need to label the numbers of the intervals
-    inters = numberIntervals(intervals)
-    rowerdata = dict(zip(inters, datalist))
-    filename.writerow(rowerdata)
+def writeRower(name, data, df):
+    output = pd.DataFrame()
+    d = {'Name': name}
+    data = d.update(data)
+    output = pd.concat([df, data], sort=False)
+    return output
+
+def createDataFrame(labels, names):
+    df = pd.DataFrame(columns=labels, index=names)
+    return df
+
+def nullNaN(df):
+    return df.fillna(0, inplace=True)
+
+def doesFileExist(filename):
+    return Path.exists(filename)
+
+def writeFile(filename, df, sheet_name='No Name', startrow=None,
+                       truncate_sheet=False, 
+                       **to_excel_kwargs):
+    # If file doesn't exist
+    if not Path.exists(filename):
+         df.to_excel(filename, sheet_name=sheet_name, 
+            startrow=startrow if startrow is not None else 0)
+    
+    else:
+        with pd.ExcelWriter(filename, mode='a', if_sheet_exists='new') as writer:
+            df.to_excel(writer, sheet_name=sheet_name, 
+                startrow=startrow if startrow is not None else 0)
+            writer.save()
+
+def getRowers(filepath = rower_path):
+    try:
+       df = pd.read_csv(filepath)
+       return df
+    except:
+        return False
 
 
     
