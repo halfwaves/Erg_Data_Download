11299065544e3772681d7710b72bcae4a275c1e9
Laura S <57412421+LauraS909@users.noreply.github.com>
2023-01-19 20:45:54 -0500

    Added some outlining

--- importscript/__main__.py
+++ importscript/__main__.py
@@ -73,9 +73,10 @@ class App(tk.Frame):
 Event handler for when user wants to remember the last session
 '''
 def rememberLastB(self):
+    # If the path doesn't exist, return false
     if not (dh.cred_path()):
-        return noCredPath
-    path = dh.getCred()['csv path']
+        return False
+    path = dh.getCred()['file path']
     if(path == ''):
         path = askdirectory(title='Please Select a destination Folder') # shows dialog box and return the path
         dh.rewriteCredentials(dh.getCred['username'],dh.getCred['password'],csv_path = path)
@@ -109,10 +110,57 @@ with open('example-workout.txt', 'r') as f:
     # p = rParser.pullIntervals()
     soup = parse.soupDammit(x)
     labels = parse.getDataLabels(soup)
-    w = dh.createFile('Is it nate', labels)
-    oop = parse.getIntervals(soup)
-    dh.writeRower('NATHANIEL', oop, w)
+    name = parse.getWorkoutDetails(soup)
+    print(name)
+    # oop = parse.getIntervals(soup)
+    # dh.writeRower('NATHANIEL', oop, w)
 
+
+#Read from internal for login credentials
+cred  = dh.getCred()
+#Get data on who to log
+rowers = dh.getRowers()
+    # If the file doesn't exist
+if not rowers:
+    # Pull all training partners 
+    rowers = wh.getTrainingPartners(s, p)
+    # Exclude Rowers
+        #TODO
+
+# Pull the first rower on the list with logged data
+labels = parse.lowLabels('example-workout.txt')
+for r in rowers:
+    urls = parse.getLogbookUrls(rowers[r])
+    if urls: #not empty
+        workout_name = list()
+        for t in urls:
+            workout = wh.getReq(s, t)
+            workout_name.append(parse.getWorkoutDetails(workout))
+        # Ask if the workout is [x/x] at date
+        #If yes: get labels from data
+
+# #Get the get all rower's workout data
+    
+    # Ask if user wants to include other workouts from that date
+    # If yes: repeat for all with the same date (write data to a new table)
+
+# Ask to exclude certain data (time, HR, split, SPM etc.)
+
+#Get names and URLS for workout from Log: Is this a sub activity 
+    # Get interval data
+    # Write to index (name) in the dataFrame
+
+# If repeat is true, concat the dataframes (so that there is only one)
+# Ask user if they have an .xlsx file for formatting:
+    # Try opening file and read Except: Return no file found error
+    # Prompt creation if doesn't exist (and ask user to name it)
+
+# Ask user for the file that the they want to write the data to 
+
+# Dataframe to xlsx
+    # With name of the tab: workout @ date
+# dh.writeFile(destination_file, data, sheet_name)
+# # Return file path of the xlsx file
 
 
 
