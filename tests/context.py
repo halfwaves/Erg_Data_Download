1461b8407ffba617b71ecf581eebdbaa87bd5b99
Laura S <57412421+LauraS909@users.noreply.github.com>
2023-01-19 20:49:11 -0500

    Restrucutred bc needlessly complicated

--- tests/context.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import sys
-import os
-print(os.path.abspath(os.path.join(os.path.dirname('__main__'))))
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname('__main__'))))
-
-import importscript
